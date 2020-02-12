3c4db13802cf1af1e8c703b32594dc7376f069a6
Cheolkie Kim <ckiekim@naver.com>
2020-02-12 16:54:55 +0900

    list user

--- view/userList.js
+++ view/userList.js
@@ -1,14 +1,14 @@
 module.exports.userList = function(rows) {
     let users = '';
     for (row of rows) {
-        users += '<tr>';
-        users += `<td>${row.uid}</td>
+        users += `<tr>
+                    <td>${row.uid}</td>
                     <td><a href="${row.name}.html">${row.name}</a></td>
                     <td>${row.deptName}</td><td>${row.tel}</td><td>${row.ts}</td>
                     <td><a href="/user/update/uid/${row.uid}"><i class="fas fa-edit"></i></a>&nbsp;&nbsp;
                         <a href="/user/password/uid/${row.uid}"><i class="fas fa-key"></i>&nbsp;&nbsp;
-                        <a href="/user/delete/uid/${row.uid}"><i class="fas fa-trash-alt"></i></td>`;
-        users += '</tr>';
+                        <a href="/user/delete/uid/${row.uid}"><i class="fas fa-trash-alt"></i></td>
+                  </tr>`;
     }
     return `
 <!DOCTYPE html>
@@ -70,25 +70,25 @@ module.exports.userList = function(rows) {
                 </ul>
             </div>
             <div class="col-10">
-            <div class="row" style="margin-left: 10px">
-            <div class="col-12"><h3>사용자 조회</h3></div>
-            <div class="col-12"><hr></div>
-            <div class="col-11">
-                <table class="table table-condensed table-hover">
-                    <thead class="thead-light">
-                        <tr class="active">
-                            <th scope="col">아이디</th><th scope="col">이름</th>
-                            <th scope="col">부서</th><th scope="col">전화번호</th>
-                            <th scope="col">등록일자</th><th scope="col">Action</th>
-                        </tr>
-                    </thead>
-                    <tbody>
-                        ${users}
-                    </tbody>
-                </table>
-            </div>
-            <div class="col-1"></div>
-        </div>
+                <div class="row" style="margin-left: 10px">
+                    <div class="col-12"><h3>사용자 조회</h3></div>
+                    <div class="col-12"><hr></div>
+                    <div class="col-11">
+                        <table class="table table-condensed table-hover">
+                            <thead class="thead-light">
+                                <tr class="active">
+                                    <th scope="col">아이디</th><th scope="col">이름</th>
+                                    <th scope="col">부서</th><th scope="col">전화번호</th>
+                                    <th scope="col">등록일자</th><th scope="col">Action</th>
+                                </tr>
+                            </thead>
+                            <tbody>
+                                ${users}
+                            </tbody>
+                        </table>
+                    </div>
+                    <div class="col-1"></div>
+                </div>
             </div>
         </div>
     </div>
