6bf4e25118e14eaf2d9bff7517c46fcfe7f9eebc
Cheolkie Kim <ckiekim@naver.com>
2020-02-12 11:52:11 +0900

    db module

---
+++ db-module.js
@@ -0,0 +1,16 @@
+const sqlite3 = require('sqlite3').verbose(); 
+
+module.exports = {
+    getAllDepts: function(callback) {
+        let db = new sqlite3.Database("db/smartfarm.db");
+        let sql = `SELECT * FROM dept`;
+        db.all(sql, function(err, rows) {
+            if (err) {
+                console.error('getAllDepts DB 오류', err);
+                return;
+            }
+            callback(rows);
+        });
+        db.close();
+    }
+}
--- userRouter.js
+++ userRouter.js
@@ -1,8 +1,27 @@
 const express = require('express');
+const dbModule = require('./db-module');
+const sqlite3 = require('sqlite3').verbose(); 
 
 const router = express.Router();
 
+router.get('/dept', function(req, res) {
+    dbModule.getAllDepts(function(rows) {
+        console.log(rows);
+    });
+    res.send('userRouter.js: dept');
+});
+
 router.get('/register', function(req, res) {
+    let db = new sqlite3.Database("db/smartfarm.db");
+    let sql = `SELECT * FROM dept`;
+    db.all(sql, function(err, rows) {
+        if (err) {
+            console.error('getAllDepts DB 오류', err);
+            return;
+        }
+        console.log(rows);
+    });
+    db.close();
     res.send('userRouter.js');
 });
 
